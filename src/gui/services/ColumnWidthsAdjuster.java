b0cdb88b8bb6e33e5aa78b20404b7179f501e654
jmoura <jmoura@jmoura-w7m.int.pason.com>
2017-04-25 13:47:33 -0600

    table row height adjustment try #1

--- src/gui/services/ColumnWidthsAdjuster.java
+++ src/gui/services/ColumnWidthsAdjuster.java
@@ -59,5 +59,21 @@ public class ColumnWidthsAdjuster {
             } else; // ???
             totalWidth += column.getPreferredWidth();
         }
+        if (SettingsManager.getInstance().isConfig("TableRowAdjust", "1", "1")) {
+            updateRowHeights(table);
+        }
+    }
+
+    private void updateRowHeights(JTable table) {
+        for (int row = 0; row < table.getRowCount(); row++) {
+            int rowHeight = table.getRowHeight();
+
+            for (int column = 0; column < table.getColumnCount(); column++) {
+                Component comp = table.prepareRenderer(table.getCellRenderer(row, column), row, column);
+                rowHeight = Math.max(rowHeight, comp.getPreferredSize().height);
+            }
+
+            table.setRowHeight(row, rowHeight);
+        }
     }
 }
