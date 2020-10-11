clashoflegends/counselor

commit 4324b3371004a51d44a1780706c81195bc02e405

Fixed OutOfMemoryError. (#46)
* add lib to path

* Try to resolve conflict

* Settings window. First part.

* Settings window. Second part, some labels.

* jspinner in settings

* jspinner in settings

* Settings window.

* Settings window.

* Settings window.

* Update code to 1.6 compatibility.

* Enable and disable auto load action and auto load turn

* Fix forecast

* Fix forecast

* Fix forecast

* Fix forecast

* Fix forecast again. I hate git.

* Fix forecast again. I hate git.

* Merge clashoflegends/master

Conflicts:
	src/gui/MainSettingsGui.form
	src/gui/MainSettingsGui.java

* Merge clashoflegends/master

Conflicts:
	src/gui/MainSettingsGui.form
	src/gui/MainSettingsGui.java

* Merge clashoflegends/master

Conflicts:
	src/gui/MainSettingsGui.form
	src/gui/MainSettingsGui.java

* Merge clashoflegends/master

Conflicts:
	src/gui/MainSettingsGui.form
	src/gui/MainSettingsGui.java

* Merge clashoflegends/master

Conflicts:
	src/gui/MainSettingsGui.form
	src/gui/MainSettingsGui.java

* Merge clashoflegends/master

Conflicts:
	src/gui/MainSettingsGui.form
	src/gui/MainSettingsGui.java

* Check box to display portraits

* Add panel to display portraits

* dispatch manager switch portrait panel

* dispatch manager switch portrait panel

* dispatch manager switch portrait panel

* dispatch manager switch portrait panel

* dispatch manager switch portrait panel

* dispatch manager switch portrait panel

* Load images from folder.

* Show ranks under portrait

* Fix window view for linux

* - Try to fix TabPersonagemGui

* Default portrait 'blank.jpg' for testing purpuse

* Revert "Default portrait 'blank.jpg' for testing purpuse"

This reverts commit 6022d873c41f3abe1ee566f95104887756c78fcc.

* Revert "Revert "Default portrait 'blank.jpg' for testing purpuse""

This reverts commit 3ca59af3ac8aa93fe2235a3d28894d7eb160eff4.

src/control/MapaControler.java
@@ -60,7 +60,7 @@ public class MapaControler extends ControlBase implements Serializable, ItemList
     private static final BundleManager labels = SettingsManager.getInstance().getBundleManager();
     private static final LocalFacade localFacade = new LocalFacade();
     private static final ExercitoFacade exercitoFacade = new ExercitoFacade();
-    private static MapaManager mapaManager; //tem que criar depois para incluir o form
+    private MapaManager mapaManager; //tem que criar depois para incluir o form
     private static final ListFactory listFactory = new ListFactory();
     private MainMapaGui tabGui;
     private DialogTextArea hexInfo;
@@ -408,4 +408,13 @@ public class MapaControler extends ControlBase implements Serializable, ItemList
         hexView.setText(text);
         hexView.setTitle(title);
     }
+
+    public MapaManager getMapaManager() {
+        return mapaManager;
+    }
+
+    public void setMapaManager(MapaManager mapaManager) {
+        this.mapaManager = mapaManager;
+    }
+    
 }

src/gui/MainResultWindowGui.java
@@ -6,6 +6,7 @@
 package gui;
 
 import business.ImageManager;
+import business.MapaManager;
 import control.MapaControler;
 import control.WorldControler;
 import control.facade.WorldFacadeCounselor;
@@ -23,8 +24,10 @@ import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JToggleButton;
 import javax.swing.plaf.basic.BasicSplitPaneUI;
+import model.Cenario;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import persistence.local.ListFactory;
 import persistenceCommons.BundleManager;
 import persistenceCommons.SettingsManager;
 import persistenceCommons.SysApoio;
@@ -503,7 +506,17 @@ public class MainResultWindowGui extends javax.swing.JPanel implements Serializa
             this.labelNacao.setText(String.format("%s: %s", labels.getString("NACAO"), wc.getNacoesJogadorAtivoQtd() + " ... "));
         }
 
-        MapaControler mapaControl = new MapaControler(this);
+        MapaControler mapaControl = WorldFacadeCounselor.getInstance().getMapaControler();//new MapaControler(this);
+        if (mapaControl == null) {
+            mapaControl = new MapaControler(this);  
+        } else {
+            final Cenario cenario = WorldFacadeCounselor.getInstance().getCenario();
+            MapaManager mapaManager = new MapaManager(cenario, this);
+            mapaManager.setLocais(new ListFactory().listLocais());
+            mapaControl.setMapaManager(mapaManager);
+        }
+
+     //   MapaControler mapaControl = new MapaControler(this);
         WorldFacadeCounselor.getInstance().setMapaControler(mapaControl);
         MainMapaGui mapaGui = new MainMapaGui(mapaControl);
         //Monta área do mapa
@@ -659,5 +672,5 @@ public class MainResultWindowGui extends javax.swing.JPanel implements Serializa
     public JToggleButton getDisplayPortraits() {
         return this.toggleDisplayPortrait;
     }
-
+        
 }

commit 4b2516ed6f03ef853b46c3c684370a24cb837790

Fixed radial menu bug introduced by #46.

src/control/MapaControler.java
@@ -58,21 +58,27 @@ public class MapaControler extends ControlBase implements Serializable, ItemList
 
     private static final Log log = LogFactory.getLog(MapaControler.class);
     private static final BundleManager labels = SettingsManager.getInstance().getBundleManager();
-    private static final LocalFacade localFacade = new LocalFacade();
-    private static final ExercitoFacade exercitoFacade = new ExercitoFacade();
+    private final LocalFacade localFacade = new LocalFacade();
+    private ExercitoFacade exercitoFacade = null;
     private MapaManager mapaManager; //tem que criar depois para incluir o form
-    private static final ListFactory listFactory = new ListFactory();
+    private ListFactory listFactory = null;
     private MainMapaGui tabGui;
     private DialogTextArea hexInfo;
     private Local localAtual;
     private final List<JTable> tables = new ArrayList<JTable>();
     private RadialMenu rmActive;
-    private final MapMenuManager mapMenuManager;
+    private MapMenuManager mapMenuManager;
     private DialogHexView hexView;
-    private final Jogador jogadorAtivo;
+    private Jogador jogadorAtivo;
 
     public MapaControler(JPanel form) {
+        initialize(form);
+    }
+
+    public void initialize(JPanel form) {
         this.jogadorAtivo = WorldFacadeCounselor.getInstance().getPartida().getJogadorAtivo();
+        listFactory = new ListFactory();
+        exercitoFacade = new ExercitoFacade();
         final Cenario cenario = WorldFacadeCounselor.getInstance().getCenario();
         mapaManager = new MapaManager(cenario, form);
         mapaManager.setLocais(listFactory.listLocais());
@@ -92,22 +98,26 @@ public class MapaControler extends ControlBase implements Serializable, ItemList
         mapMenuManager.setLocais(listFactory.listLocais());
         mapMenuManager.setNacoes(listFactory.listNacoes());
         mapMenuManager.setTerrenos(cenario.getTerrenos());
+        rmActive = getMapMenuManager().getMainMenu();
+        
+        
+        
     }
-
+    
     private ImageIcon printActionsOnMap() {
-        return new ImageIcon(mapaManager.printActionsOnMap(listFactory.listLocais().values(), listFactory.listPersonagens(), jogadorAtivo));
+        return new ImageIcon(mapaManager.printActionsOnMap(listFactory.listLocais().values(), listFactory.listPersonagens(), getJogadorAtivo()));
     }
 
     public ImageIcon printMapaGeral() {
-        return new ImageIcon(mapaManager.printMapaGeral(listFactory.listLocais().values(), listFactory.listPersonagens(), jogadorAtivo));
+        return new ImageIcon(mapaManager.printMapaGeral(listFactory.listLocais().values(), listFactory.listPersonagens(), getJogadorAtivo()));
     }
 
     private ImageIcon refreshMapaGeral() {
-        return new ImageIcon(mapaManager.redrawMapaGeral(listFactory.listLocais().values(), listFactory.listPersonagens(), jogadorAtivo));
+        return new ImageIcon(mapaManager.redrawMapaGeral(listFactory.listLocais().values(), listFactory.listPersonagens(), getJogadorAtivo()));
     }
 
     public BufferedImage getMap() {
-        return mapaManager.printMapaGeral(listFactory.listLocais().values(), listFactory.listPersonagens(), jogadorAtivo);
+        return mapaManager.printMapaGeral(listFactory.listLocais().values(), listFactory.listPersonagens(), getJogadorAtivo());
     }
 
     public MainMapaGui getTabGui() {
@@ -222,7 +232,7 @@ public class MapaControler extends ControlBase implements Serializable, ItemList
                     hideRadialMenu();
                     rmActive = WorldBuilderMenuManager.getInstance().getRmWorldBuilder();
                 } else if (rmActive == null) {
-                    rmActive = mapMenuManager.getMainMenu();
+                    rmActive = getMapMenuManager().getMainMenu();
                 }
                 this.printTag(local);
             }
@@ -352,7 +362,7 @@ public class MapaControler extends ControlBase implements Serializable, ItemList
         if (rmActive == null && SettingsManager.getInstance().isWorldBuilder()) {
             rmActive = WorldBuilderMenuManager.getInstance().getRmWorldBuilder();
         } else if (rmActive == null) {
-            rmActive = mapMenuManager.getMainMenu();
+            rmActive = getMapMenuManager().getMainMenu();
         }
         rmActive.setHierarchyAncestor(this);
 
@@ -416,5 +426,35 @@ public class MapaControler extends ControlBase implements Serializable, ItemList
     public void setMapaManager(MapaManager mapaManager) {
         this.mapaManager = mapaManager;
     }
+
+    /**
+     * @return the mapMenuManager
+     */
+    public MapMenuManager getMapMenuManager() {
+        return mapMenuManager;
+    }
+
+    /**
+     * @param mapMenuManager the mapMenuManager to set
+     */
+    public void setMapMenuManager(MapMenuManager mapMenuManager) {
+        this.mapMenuManager = mapMenuManager;
+    }
+
+    /**
+     * @return the jogadorAtivo
+     */
+    public Jogador getJogadorAtivo() {
+        return jogadorAtivo;
+    }
+
+    /**
+     * @param jogadorAtivo the jogadorAtivo to set
+     */
+    public void setJogadorAtivo(Jogador jogadorAtivo) {
+        this.jogadorAtivo = jogadorAtivo;
+    }
+
+    
     
 }

src/gui/MainDadosGui.java
@@ -34,8 +34,9 @@ public class MainDadosGui extends javax.swing.JPanel implements Serializable {
     /**
      * Creates new form MainDadosGui
      */
-    public MainDadosGui(MapaControler mapaControl) {
+    public MainDadosGui() {
         initComponents();
+        MapaControler mapaControl = WorldFacadeCounselor.getInstance().getMapaControler();
         // prepara as tabs
         if (!SettingsManager.getInstance().isConfig("SetTabPosition", "1", "1")) {
             this.jTabbedPane1.setTabPlacement(SettingsManager.getInstance().getConfigAsInt("SetTabPosition"));

src/gui/MainMapaGui.java
@@ -11,6 +11,7 @@ import business.MovimentoExercito;
 import business.converter.ConverterFactory;
 import business.services.TagManager;
 import control.MapaControler;
+import control.facade.WorldFacadeCounselor;
 import control.services.MapaDragListener;
 import java.awt.AlphaComposite;
 import java.awt.Color;
@@ -54,9 +55,9 @@ public final class MainMapaGui extends javax.swing.JPanel implements Serializabl
     /**
      * Creates new form MainMapaGui
      */
-    public MainMapaGui(MapaControler mapaControl) {
+    public MainMapaGui() {
         initComponents();
-
+        MapaControler mapaControl = WorldFacadeCounselor.getInstance().getMapaControler();
         mapaControl.setTabGui(this);
         doMapa(mapaControl.printMapaGeral());
         this.mapaLabel.addMouseMotionListener(new MapaDragListener(this));

src/gui/MainResultWindowGui.java
@@ -19,18 +19,22 @@ import java.awt.Dimension;
 import java.awt.event.ComponentEvent;
 import java.awt.event.ComponentListener;
 import java.io.Serializable;
+import java.util.SortedMap;
 import javax.swing.GroupLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JToggleButton;
 import javax.swing.plaf.basic.BasicSplitPaneUI;
 import model.Cenario;
+import model.Local;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import persistence.local.ListFactory;
 import persistenceCommons.BundleManager;
 import persistenceCommons.SettingsManager;
 import persistenceCommons.SysApoio;
+import radialMenu.mapmenu.MapMenuManager;
+import radialMenu.worldBuilder.WorldBuilderMenuManager;
 
 /**
  *
@@ -506,23 +510,20 @@ public class MainResultWindowGui extends javax.swing.JPanel implements Serializa
             this.labelNacao.setText(String.format("%s: %s", labels.getString("NACAO"), wc.getNacoesJogadorAtivoQtd() + " ... "));
         }
 
-        MapaControler mapaControl = WorldFacadeCounselor.getInstance().getMapaControler();//new MapaControler(this);
+        MapaControler mapaControl = WorldFacadeCounselor.getInstance().getMapaControler();//new MapaControler(this); //
         if (mapaControl == null) {
             mapaControl = new MapaControler(this);  
         } else {
-            final Cenario cenario = WorldFacadeCounselor.getInstance().getCenario();
-            MapaManager mapaManager = new MapaManager(cenario, this);
-            mapaManager.setLocais(new ListFactory().listLocais());
-            mapaControl.setMapaManager(mapaManager);
+            mapaControl.initialize(this);
         }
 
      //   MapaControler mapaControl = new MapaControler(this);
         WorldFacadeCounselor.getInstance().setMapaControler(mapaControl);
-        MainMapaGui mapaGui = new MainMapaGui(mapaControl);
+        MainMapaGui mapaGui = new MainMapaGui();
         //Monta área do mapa
         this.splitMainPanel.setRightComponent(mapaGui);
         // Monta tabbed panel para dados
-        MainDadosGui dadosGui = new MainDadosGui(mapaControl);
+        MainDadosGui dadosGui = new MainDadosGui();
         this.splitMainPanel.setLeftComponent(dadosGui);
         this.tabPersonagem = dadosGui.getTabPersonagem();
 