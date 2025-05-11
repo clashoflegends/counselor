clashoflegends/counselor

commit 4324b3371004a51d44a1780706c81195bc02e405

Fixed OutOfMemoryError. (#46)
* add lib to path

* Try to resolve conflict

* Settings window. First part.

* Settings window. Second part, some labels.

* jspinner in settings

* jspinner in settings

* Settings window.

* Settings window.

* Settings window.

* Update code to 1.6 compatibility.

* Enable and disable auto load action and auto load turn

* Fix forecast

* Fix forecast

* Fix forecast

* Fix forecast

* Fix forecast again. I hate git.

* Fix forecast again. I hate git.

* Merge clashoflegends/master

Conflicts:
	src/gui/MainSettingsGui.form
	src/gui/MainSettingsGui.java

* Merge clashoflegends/master

Conflicts:
	src/gui/MainSettingsGui.form
	src/gui/MainSettingsGui.java

* Merge clashoflegends/master

Conflicts:
	src/gui/MainSettingsGui.form
	src/gui/MainSettingsGui.java

* Merge clashoflegends/master

Conflicts:
	src/gui/MainSettingsGui.form
	src/gui/MainSettingsGui.java

* Merge clashoflegends/master

Conflicts:
	src/gui/MainSettingsGui.form
	src/gui/MainSettingsGui.java

* Merge clashoflegends/master

Conflicts:
	src/gui/MainSettingsGui.form
	src/gui/MainSettingsGui.java

* Check box to display portraits

* Add panel to display portraits

* dispatch manager switch portrait panel

* dispatch manager switch portrait panel

* dispatch manager switch portrait panel

* dispatch manager switch portrait panel

* dispatch manager switch portrait panel

* dispatch manager switch portrait panel

* Load images from folder.

* Show ranks under portrait

* Fix window view for linux

* - Try to fix TabPersonagemGui

* Default portrait 'blank.jpg' for testing purpuse

* Revert "Default portrait 'blank.jpg' for testing purpuse"

This reverts commit 6022d873c41f3abe1ee566f95104887756c78fcc.

* Revert "Revert "Default portrait 'blank.jpg' for testing purpuse""

This reverts commit 3ca59af3ac8aa93fe2235a3d28894d7eb160eff4.

src/gui/MainResultWindowGui.java
@@ -6,6 +6,7 @@
 package gui;
 
 import business.ImageManager;
+import business.MapaManager;
 import control.MapaControler;
 import control.WorldControler;
 import control.facade.WorldFacadeCounselor;
@@ -23,8 +24,10 @@ import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JToggleButton;
 import javax.swing.plaf.basic.BasicSplitPaneUI;
+import model.Cenario;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import persistence.local.ListFactory;
 import persistenceCommons.BundleManager;
 import persistenceCommons.SettingsManager;
 import persistenceCommons.SysApoio;
@@ -503,7 +506,17 @@ public class MainResultWindowGui extends javax.swing.JPanel implements Serializa
             this.labelNacao.setText(String.format("%s: %s", labels.getString("NACAO"), wc.getNacoesJogadorAtivoQtd() + " ... "));
         }
 
-        MapaControler mapaControl = new MapaControler(this);
+        MapaControler mapaControl = WorldFacadeCounselor.getInstance().getMapaControler();//new MapaControler(this);
+        if (mapaControl == null) {
+            mapaControl = new MapaControler(this);  
+        } else {
+            final Cenario cenario = WorldFacadeCounselor.getInstance().getCenario();
+            MapaManager mapaManager = new MapaManager(cenario, this);
+            mapaManager.setLocais(new ListFactory().listLocais());
+            mapaControl.setMapaManager(mapaManager);
+        }
+
+     //   MapaControler mapaControl = new MapaControler(this);
         WorldFacadeCounselor.getInstance().setMapaControler(mapaControl);
         MainMapaGui mapaGui = new MainMapaGui(mapaControl);
         //Monta área do mapa
@@ -659,5 +672,5 @@ public class MainResultWindowGui extends javax.swing.JPanel implements Serializa
     public JToggleButton getDisplayPortraits() {
         return this.toggleDisplayPortrait;
     }
-
+        
 }

src/control/MapaControler.java
@@ -60,7 +60,7 @@ public class MapaControler extends ControlBase implements Serializable, ItemList
     private static final BundleManager labels = SettingsManager.getInstance().getBundleManager();
     private static final LocalFacade localFacade = new LocalFacade();
     private static final ExercitoFacade exercitoFacade = new ExercitoFacade();
-    private static MapaManager mapaManager; //tem que criar depois para incluir o form
+    private MapaManager mapaManager; //tem que criar depois para incluir o form
     private static final ListFactory listFactory = new ListFactory();
     private MainMapaGui tabGui;
     private DialogTextArea hexInfo;
@@ -408,4 +408,13 @@ public class MapaControler extends ControlBase implements Serializable, ItemList
         hexView.setText(text);
         hexView.setTitle(title);
     }
+
+    public MapaManager getMapaManager() {
+        return mapaManager;
+    }
+
+    public void setMapaManager(MapaManager mapaManager) {
+        this.mapaManager = mapaManager;
+    }
+    
 }

commit 2e979665e8df28cd76f41cbcf2598e1bb87b74ea

Fix layout issue in the main data panel. (#53)
* add lib to path

* Try to resolve conflict

* Settings window. First part.

* Settings window. Second part, some labels.

* jspinner in settings

* jspinner in settings

* Settings window.

* Settings window.

* Settings window.

* Update code to 1.6 compatibility.

* Enable and disable auto load action and auto load turn

* Fix forecast

* Fix forecast

* Fix forecast

* Fix forecast

* Fix forecast again. I hate git.

* Fix forecast again. I hate git.

* Merge clashoflegends/master

Conflicts:
	src/gui/MainSettingsGui.form
	src/gui/MainSettingsGui.java

* Merge clashoflegends/master

Conflicts:
	src/gui/MainSettingsGui.form
	src/gui/MainSettingsGui.java

* Merge clashoflegends/master

Conflicts:
	src/gui/MainSettingsGui.form
	src/gui/MainSettingsGui.java

* Merge clashoflegends/master

Conflicts:
	src/gui/MainSettingsGui.form
	src/gui/MainSettingsGui.java

* Merge clashoflegends/master

Conflicts:
	src/gui/MainSettingsGui.form
	src/gui/MainSettingsGui.java

* Merge clashoflegends/master

Conflicts:
	src/gui/MainSettingsGui.form
	src/gui/MainSettingsGui.java

* Check box to display portraits

* Add panel to display portraits

* dispatch manager switch portrait panel

* dispatch manager switch portrait panel

* dispatch manager switch portrait panel

* dispatch manager switch portrait panel

* dispatch manager switch portrait panel

* dispatch manager switch portrait panel

* Load images from folder.

* Show ranks under portrait

* Fix window view for linux

* - Try to fix TabPersonagemGui

* Default portrait 'blank.jpg' for testing purpuse

* Revert "Default portrait 'blank.jpg' for testing purpuse"

This reverts commit 6022d873c41f3abe1ee566f95104887756c78fcc.

* Revert "Revert "Default portrait 'blank.jpg' for testing purpuse""

This reverts commit 3ca59af3ac8aa93fe2235a3d28894d7eb160eff4.

* Revert "Revert "Revert "Default portrait 'blank.jpg' for testing purpuse"""

This reverts commit 01fbd3cc6e922d023d413a6bf8c67043e4812018.

* Revert "- Try to fix TabPersonagemGui"

This reverts commit 5c6e52b6c0de632f1657e1a45df63be543309c82.

* Default portrait 'blank.jpg' for testing purpuse

* - User choose portraits folder.

* - Download portrait file with progress monitor.

* - Download portrait file with progress monitor.

* - Download portrait file with progress monitor.

* - Fix delete zip file after unzip process.

* Try to fix the issue with config window in dual monitor.

* Remove config files

* Remove config files. Commit unchecked

* Remove config files. Commit unchecked

* Restore config files to commit 804ed22

* Remove temp files

* - Fix incorrect jpanel resize for items with long history.

* - Fix incorrect jpanel resize for items with long history.

* - Fix incorrect jpanel resize for items with long history.

* Revert changes

* Reveert

* Manual sync from clash

* - Download folder selector moved
- Fix enable portraits checkbox behavior when portraits folder is accidentall deleted.
- Enable portraits checkbox is correctly enabled when de download process is finished.

* Revert config files

* Revert config files

* Revert unwanted changed file

* Wrap width in the panel of orders.

* Added portrait folder text field in settings window.

* src/control/services/DownloadPortraitsHttpServiceImpl.java

* New toggle buttons added in the main tool bar. To testing.

* Fixed Java version 8 detection.

* Fixed OutOfMemoryException crash when trying to open multiple turn files in the same Counselor instance.

* Fixed radial menu bug introduced by #46.

src/control/WorldControler.java
@@ -1278,8 +1278,10 @@ public class WorldControler extends ControlBase implements Serializable, ActionL
             progressMonitor = new ProgressMonitor(gui, "Downloading file...", "", 0, 100);
             progressMonitor.setProgress(0);
             displayPortraitsManager.downloadPortraits(gui, this);
+            
+        } else {
+        	doDisplayPortraits(button);
         }
-        doDisplayPortraits(button);
     }
 
     private int calculateDrawPcPathValue(int pcPath, int pcPathFuture) {

src/control/support/DisplayPortraitsManager.java
@@ -72,6 +72,8 @@ public class DisplayPortraitsManager {
             if (jFileReturn == JFileChooser.APPROVE_OPTION) {
                 File file = folderChooser.getSelectedFile();
                 SettingsManager.getInstance().setConfig("PortraitsFolder", file.getPath());
+                SettingsManager.getInstance().setConfig("ShowCharacterPortraits", String.valueOf(1));
+                SettingsManager.getInstance().doConfigSave("ShowCharacterPortraits");
 
             } else {
                 throw new BusinessException();

src/gui/MainResultWindowGui.java
@@ -792,7 +792,7 @@ public class MainResultWindowGui extends javax.swing.JPanel implements Serializa
     }
 
     public boolean isDisplayPortraitsSelected() {
-        return settingsManager.getConfig("ShowCharacterPortraits", "1").equals("1");
+        return settingsManager.getConfig("ShowCharacterPortraits", "0").equals("1");
     }
 
     public JToggleButton getDisplayPortraits() {

src/gui/accessories/DownloadProgressWork.java
@@ -7,6 +7,8 @@ package gui.accessories;
 
 import business.DownloadPortraitsService;
 import control.services.DownloadPortraitsHttpServiceImpl;
+import control.support.DispatchManager;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -75,6 +77,8 @@ public class DownloadProgressWork extends SwingWorker<Void, Void> {
         downloadFile.delete();
         String successLabel = "Successful download and uncompress process. A total of " + filesCount + " portraits have been obtained.";
         JOptionPane.showMessageDialog(null, successLabel);
+        DispatchManager.getInstance().sendDispatchForMsg(DispatchManager.SWITCH_PORTRAIT_PANEL, String.valueOf(1));
+        DispatchManager.getInstance().sendDispatchForMsg(DispatchManager.ACTIONS_RELOAD, "");        
 
     }
 

src/gui/tabs/TabPersonagensGui.java
@@ -33,6 +33,9 @@ import persistenceCommons.BundleManager;
 import persistenceCommons.SettingsManager;
 import persistenceCommons.SysApoio;
 import utils.OpenSlotCounter;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.GroupLayout;
+import javax.swing.LayoutStyle.ComponentPlacement;
 
 /**
  *
@@ -210,20 +213,20 @@ public class TabPersonagensGui extends TabBase implements Serializable, IAcaoGui
         portraitPanel.setVisible(showPortrait);
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
-        jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                .addGap(1, 1, 1)
-                .addComponent(portraitPanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(detalhesPersonagem, javax.swing.GroupLayout.DEFAULT_SIZE, 500, Short.MAX_VALUE))
+        	jPanel1Layout.createParallelGroup(Alignment.TRAILING)
+        		.addGroup(jPanel1Layout.createSequentialGroup()
+        			.addGap(6)
+        			.addComponent(portraitPanel, GroupLayout.PREFERRED_SIZE, 120, GroupLayout.PREFERRED_SIZE)
+        			.addPreferredGap(ComponentPlacement.RELATED)
+        			.addComponent(detalhesPersonagem, GroupLayout.DEFAULT_SIZE, 401, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(portraitPanel, javax.swing.GroupLayout.DEFAULT_SIZE, 246, Short.MAX_VALUE)
-            .addComponent(detalhesPersonagem, javax.swing.GroupLayout.DEFAULT_SIZE, 246, Short.MAX_VALUE)
+        	jPanel1Layout.createParallelGroup(Alignment.LEADING)
+        		.addComponent(portraitPanel, GroupLayout.DEFAULT_SIZE, 104, Short.MAX_VALUE)
+        		.addComponent(detalhesPersonagem, GroupLayout.DEFAULT_SIZE, 104, Short.MAX_VALUE)
         );
+        jPanel1.setLayout(jPanel1Layout);
 
         detalhesPersonagem.getAccessibleContext().setAccessibleName("Ações");
 

src/gui/tabs/TabTipoTropasGui.java
@@ -21,6 +21,8 @@ import org.apache.commons.logging.LogFactory;
 import persistence.local.WorldManager;
 import persistenceCommons.BundleManager;
 import persistenceCommons.SettingsManager;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.LayoutStyle.ComponentPlacement;
 
 /**
  *
@@ -88,39 +90,39 @@ public class TabTipoTropasGui extends TabBase {
         jLabel4.setText(bundle.getString("TAB.SEARCH.LABEL")); // NOI18N
 
         javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
-        jPanel2.setLayout(jPanel2Layout);
         jPanel2Layout.setHorizontalGroup(
-            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel2Layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jLabel1)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(comboFiltro, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, 18)
-                .addComponent(jLabel4)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(searchField, javax.swing.GroupLayout.PREFERRED_SIZE, 92, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(jLabel2)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(qtTropas)
-                .addContainerGap())
+        	jPanel2Layout.createParallelGroup(Alignment.LEADING)
+        		.addGroup(jPanel2Layout.createSequentialGroup()
+        			.addContainerGap()
+        			.addComponent(jLabel1)
+        			.addPreferredGap(ComponentPlacement.RELATED)
+        			.addComponent(comboFiltro, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+        			.addGap(18)
+        			.addComponent(jLabel4)
+        			.addPreferredGap(ComponentPlacement.RELATED)
+        			.addComponent(searchField, GroupLayout.PREFERRED_SIZE, 92, GroupLayout.PREFERRED_SIZE)
+        			.addPreferredGap(ComponentPlacement.RELATED, 118, Short.MAX_VALUE)
+        			.addComponent(jLabel2)
+        			.addPreferredGap(ComponentPlacement.UNRELATED)
+        			.addComponent(qtTropas)
+        			.addContainerGap())
         );
         jPanel2Layout.setVerticalGroup(
-            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel2Layout.createSequentialGroup()
-                .addContainerGap()
-                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                        .addComponent(searchField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addComponent(jLabel4))
-                    .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                        .addComponent(comboFiltro, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addComponent(jLabel1)
-                        .addComponent(jLabel2)
-                        .addComponent(qtTropas)))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        	jPanel2Layout.createParallelGroup(Alignment.LEADING)
+        		.addGroup(jPanel2Layout.createSequentialGroup()
+        			.addContainerGap()
+        			.addGroup(jPanel2Layout.createParallelGroup(Alignment.LEADING)
+        				.addGroup(jPanel2Layout.createParallelGroup(Alignment.BASELINE)
+        					.addComponent(searchField, GroupLayout.DEFAULT_SIZE, 26, Short.MAX_VALUE)
+        					.addComponent(jLabel4))
+        				.addGroup(jPanel2Layout.createParallelGroup(Alignment.BASELINE)
+        					.addComponent(comboFiltro, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+        					.addComponent(jLabel1)
+        					.addComponent(qtTropas)
+        					.addComponent(jLabel2)))
+        			.addGap(0))
         );
+        jPanel2.setLayout(jPanel2Layout);
 
         jSplitPane1.setBorder(null);
         jSplitPane1.setDividerLocation(SettingsManager.getInstance().getConfigAsInt("troopsSplitSize", "200"));
@@ -296,19 +298,19 @@ public class TabTipoTropasGui extends TabBase {
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
-        this.setLayout(layout);
         layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-            .addComponent(jPanel1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        	layout.createParallelGroup(Alignment.LEADING)
+        		.addComponent(jPanel2, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        		.addComponent(jPanel1, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, 450, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        	layout.createParallelGroup(Alignment.TRAILING)
+        		.addGroup(layout.createSequentialGroup()
+        			.addComponent(jPanel2, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+        			.addPreferredGap(ComponentPlacement.RELATED)
+        			.addComponent(jPanel1, GroupLayout.DEFAULT_SIZE, 257, Short.MAX_VALUE))
         );
+        this.setLayout(layout);
     }// </editor-fold>//GEN-END:initComponents
 
     private void jSplitPane1PropertyChange(java.beans.PropertyChangeEvent evt) {//GEN-FIRST:event_jSplitPane1PropertyChange